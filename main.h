98c001867d880992a82a6371b57fb80679aa8156
cecilacecsoh <ceciloluoch95@gmail.com>
2023-06-27 08:39:14 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,23 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * struct print - the struct for printer functions.
+ * @type_arg: identifier
+ * @f: pointer to a printer functions
+ *
+ * Description: struct that stores pointers to a
+ * printf functions.
+ */
+typedef struct print
+{
+	char *type_arg;
+	int (*f)(va_list, char *, unsigned int);
+} print_t;
+
+
+int print_chrStrPct(const char *format, ...);
